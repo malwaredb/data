d6c1ab04b4d9b0fe270469e51e4aa66493ca2ff2
Richard Zak <richard.j.zak@gmail.com>
2023-09-25 20:38:10 -0400

    feat: add VirusShare_00481.zip
    Signed-off-by: Richard Zak <richard.j.zak@gmail.com>

---
+++ sources/VirusShare_00481.zip.sql
@@ -0,0 +1,1 @@
+INSERT INTO source(name, description, url, firstacquisition, releasable) VALUES('VirusShare_00481.zip', 'VirusShare_00481.zip, 24.11 GB', 'https://virusshare.com/', '2023-09-22 13:51:57 -04:00', true);
